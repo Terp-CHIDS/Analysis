d9dbe1697f6862814e432d5c9fdbe187f75fb713
Akanksha Bapna <akanksha.bapna@marylandsmith.umd.edu>
2021-07-07 10:38:23 -0400

    importing merged dataset

--- chids.R
+++ chids.R
@@ -2,6 +2,8 @@ setwd("/Users/akanksha/Documents/GitHub/Analysis")
 
 # Libraries ####
 library(readxl)
+library(utils)
+library(tibble)
 #
 # Data Import #### 
 Dating_Apps_Respondent_Data_CORE <- read_excel("Dating Apps_Respondent_Data_CORE.xlsx")
@@ -9,9 +11,9 @@ Dont_be_Fooled_Respondent_Data_CORE <- read_excel("Dont be Fooled_Respondent_Dat
 Fealessness_Respondent_Data_CORE <- read_excel("Fealessness_Respondent_Data_CORE.xlsx")
 Read_my_lips_Respondent_Data_CORE <- read_excel("Read my lips_Respondent_Data_CORE.xlsx")
 She_is_Valuable_Respondent_Data_CORE <- read_excel("She is Valuable_Respondent_Data_CORE.xlsx")
+Merged <- read_excel("Merged.xlsx")
 #
-# Summary ####
-summary(Dating_Apps_Respondent_Data_CORE)
+# summary ####
+view(Merged)
+#  ####
 
-#new org
-#testing branch....
