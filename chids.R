1e9a706246b0e57dc94fed5ce03bdc217e5185a2
Akanksha Bapna <akanksha.bapna@marylandsmith.umd.edu>
2021-07-14 11:01:16 -0400

    pca

--- chids.R
+++ chids.R
@@ -12,6 +12,7 @@ Fealessness_Respondent_Data_CORE <- read_excel("Fealessness_Respondent_Data_CORE
 Read_my_lips_Respondent_Data_CORE <- read_excel("Read my lips_Respondent_Data_CORE.xlsx")
 She_is_Valuable_Respondent_Data_CORE <- read_excel("She is Valuable_Respondent_Data_CORE.xlsx")
 Merged <- read_excel("Merged.xlsx")
+pca.data <- read.csv("data_pca_clean.csv", header = T, sep = ',')
 #
 # summary ####
 view(Merged)
@@ -20,3 +21,20 @@ summary(Merged)
 # cleaning the dataset ####
 
 
+
+# PCA ####
+pca.data.sc <- pca.data
+pca.data.sc [,] <- data.frame(scale(pca.data[,]))
+#aggregate mean ratings by brand
+pca.data.mean <- aggregate(.~ , data = pca.data.sc, mean)
+#housekeeping -- 
+rownames(pca.data.mean) <- pca.data.mean[, 1]
+pca.data.mean <- pca.data.mean[, ]
+#visualizing pca
+pca.data.pc <- prcomp(pca.data.sc[,])
+#scree or elbow plot
+plot(pca.data.pc, type= "l")
+#mean data Q2 
+pca.data.mean.pc <- prcomp(pca.data.mean, scale = TRUE)
+biplot(pca.data.mean.pc, main ="pca")
+biplot(pca.data.mean.pc, choices = 2:3)
