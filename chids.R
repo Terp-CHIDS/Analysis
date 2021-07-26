d7a76835a1a027b84eb7d477e733d825f82a84c4
Akanksha Bapna <akanksha.bapna@marylandsmith.umd.edu>
2021-07-25 23:41:29 -0400

    pca predictor variables

--- chids.R
+++ chids.R
@@ -34,7 +34,7 @@ pca <- subset(pca, select = -c(Mood_tone))
 #pca[is.na(pca)] = 0
 pca <- replace(pca, is.na(pca), 0)
 
-# PCA ####
+# PCA on the entire dataset ####
 pca.sc <- pca
 pca.sc [,3:87] <- data.frame(scale(pca[,3:87]))
 pca.pc <- prcomp(pca.sc[,3:87], scale = T, center = T)
@@ -42,13 +42,7 @@ summary(pca.pc)
 fviz_eig(pca.pc)
 #visualizing PCA
 plot(pca.pc, type = "l")
-#aggregate mean ratings by brand ####
-#pca.mean <- aggregate(.~Ads , data = pca.sc, mean)
-#housekeeping -- 
-#rownames(pca.mean) <- pca.mean[, 1]
-#pca.data.mean <- pca.data.mean[, ]
-
-# Graph of individuals. Individuals with a similar profile are grouped together. ####
+# Graph of individuals. Individuals with a similar profile are grouped together. 
 
 fviz_pca_ind(pca.pc,
              col.ind = "cos2", # Color by the quality of representation
@@ -89,4 +83,74 @@ res.ind$coord          # Coordinates
 res.ind$contrib        # Contributions to the PCs
 res.ind$cos2           # Quality of representation 
 
+# PCA on predictor variables only ####
 
+# independent variables : 
+# Age | ACTIVEINVOLVEMENT_1 | ACTIVEINVOLVEMENT_2 | ACTIVEINVOLVEMENT_3 | AD_DISTINCTIVENESS | UNDERSTANDING 
+# RELEVANCE | CREDIBILITY | WATCHSKIP | STOPLOOK | NEWINFORMATION | 
+# Motherhood..:.Interest | Parenting..:.Interest | Reading..:.Interest | Word.Games..:.Interest | 
+# Charities.And.Causes..:.Interest | Vacations..:.Interest | Live.Events..:.Interest | Adventure.Travel..:.Interest
+# Climate.Change..:.ISSUE | Drug.Usage.Amongst.Young.People..:.ISSUE | Having.A.Healthier.Lifestyle..:.ISSUE | 
+# Making.Your.Neighbourhood.A.Safer.Place..:.ISSUE | Reducing.Road.Accidents..:.ISSUE | The.Amount.Of.Alcohol.Consumed.By.People..:.ISSUE
+# The.Effects.Of.Smoking.And.Passive.Smoking..:.ISSUE | Crime.Prevention..:.ISSUE | Unemployment.Or.Job.Security..:.ISSUE |
+# Gender-based.Violence..:.ISSUE | HIV.Prevention..:.ISSUE
+
+pca.predictor.var <- c("Ads", "Age", "ACTIVEINVOLVEMENT_1", "ACTIVEINVOLVEMENT_2", "ACTIVEINVOLVEMENT_3", "AD_DISTINCTIVENESS", "UNDERSTANDING",
+                                        "RELEVANCE", "CREDIBILITY", "WATCHSKIP", "STOPLOOK", "NEWINFORMATION", "Motherhood....Interest" ,
+                                        "Parenting....Interest", "Reading....Interest", "Word.Games....Interest", 
+                                        "Charities.And.Causes....Interest", "Vacations....Interest", "Live.Events....Interest",
+                                        "Adventure.Travel....Interest", "Climate.Change....ISSUE", 
+                                        "Drug.Usage.Amongst.Young.People....ISSUE", "Having.A.Healthier.Lifestyle....ISSUE", 
+                                        "Making.Your.Neighbourhood.A.Safer.Place....ISSUE",
+                                        "Reducing.Road.Accidents....ISSUE", "The.Amount.Of.Alcohol.Consumed.By.People....ISSUE", 
+                                        "The.Effects.Of.Smoking.And.Passive.Smoking....ISSUE", "Crime.Prevention....ISSUE",
+                                        "Unemployment.Or.Job.Security....ISSUE", "Gender.based.Violence....ISSUE", "HIV.Prevention....ISSUE")
+pca.predictor <- pca[pca.predictor.var]
+pca.predictor [,1:31] <- data.frame(scale(pca.predictor[,1:31]))
+pca.predictor <- prcomp(pca.predictor[,1:31], scale = T, center = T)
+summary(pca.predictor)
+fviz_eig(pca.predictor)
+
+fviz_pca_ind(pca.predictor,
+             col.ind = "cos2", # Color by the quality of representation
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = T    #  Avoid text overlapping
+)
+
+# Graph of variables. Positive correlated variables point to the same side of the plot. Negative correlated variables point to opposite sides of the graph.
+
+fviz_pca_var(pca.predictor,
+             col.var = "contrib", # Color by contributions to the PC
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+
+# Biplot of individuals and variables
+
+fviz_pca_biplot(pca.predictor, 
+                repel = TRUE,
+                col.var = "#2E9FDF", # Variables color
+                col.ind = "#696969"  # Individuals color
+)
+
+#Access to the PCA results
+
+# Eigenvalues
+eig.val <- get_eigenvalue(pca.predictor)
+eig.val
+
+# Results for Variables
+res.var <- get_pca_var(pca.predictor)
+res.var$coord          # Coordinates
+res.var$contrib        # Contributions to the PCs
+res.var$cos2           # Quality of representation 
+# Results for individuals
+res.ind <- get_pca_ind(pca.predictor)
+res.ind$coord          # Coordinates
+res.ind$contrib        # Contributions to the PCs
+res.ind$cos2           # Quality of representation 
+
+# PCA on Indicator variables only ####
+
+# dependent variables :
+# Branding | HIV Prevention Persuasion 2
